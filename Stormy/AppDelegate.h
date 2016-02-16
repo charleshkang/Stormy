2bb91c0a3ec272937cf956fbb30c8cde311b523f
charles <charleshkang1@gmail.com>
2016-02-16 00:45:27 -0500

    first commit

---
+++ Stormy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stormy
+//
+//  Created by Charles Kang on 2/16/16.
+//  Copyright © 2016 Charles Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stormy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stormy
+//
+//  Created by Charles Kang on 2/16/16.
+//  Copyright © 2016 Charles Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
